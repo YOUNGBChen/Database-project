a7176aa158de04165825575aed730afb33408ec2
YOUNGBC <455288561@qq.com>
2020-06-22 10:19:51 +0800

    1

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class information(models.Model):
+    isbn = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    publish = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    sort = models.CharField(max_length=30)
+    year = models.CharField(max_length=30)
+
+class users(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
