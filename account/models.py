8e8366a3602cc8f7a96303bdd0f0cd78edaa3f1b
YOUNGBC <455288561@qq.com>
2020-06-24 09:40:18 +0800

    登陆

--- account/models.py
+++ account/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
-
 class User(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    ID = models.CharField(max_length=30)
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30,null=True)
+    password = models.CharField(max_length=30,null=True)
     def __str__(self):
         return self.username
+
+
--- account/views.py
+++ account/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
+from .models import User
+from django.http import HttpResponse
 
 # Create your views here.
 from account import models
 
 def register(request):
-    # if request.method == "POST":
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    ID = request.POST.get('ID')
+    if request.method == "POST":
+        new_username = request.POST.get('username')
+        new_password = request.POST.get('password')
+        nwe_id = request.POST.get('id')
+        #new_user=models.User(username = new_username,password = new_password,id=nwe_id)
+        #new_user.save()
+        models.User.objects.create(
+            username=new_username,
+            password=new_password,
+            id=nwe_id,
+        )
+        user_lis = models.User.objects.all()
     return render(request,"register.html")
 
 def detail(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if User.objects.filter(username=username):
+            user = User.objects.filter(username = username,password = password)
+            if user:
+                return HttpResponse('登陆成功')
+            else:
+                return HttpResponse("密码错误，请尝试重新登陆")
+        else:
+            return HttpResponse("用户名不存在，请返回注册")
     return render(request,"login.html")
