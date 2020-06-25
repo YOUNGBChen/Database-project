ab4034ee4508586506cc7024b0cbd21fdee3c2ad
YOUNGBC <455288561@qq.com>
2020-06-25 16:53:59 +0800

    登记图书

--- account/models.py
+++ account/models.py
@@ -6,6 +6,9 @@ class User(models.Model):
     username = models.CharField(max_length=30,null=True)
     password = models.CharField(max_length=30,null=True)
     idcard = models.CharField(max_length=30, null=True)
+    borrowed = models.IntegerField(max_length=5, null=True)
+    staff = models.IntegerField(max_length=2, null=True)
+    deposit = models.IntegerField(max_length=5, null=True)
     def __str__(self):
         return self.username
 
